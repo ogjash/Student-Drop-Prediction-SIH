ef6f3c85345d562a1b76040e500242068bf050a0
ogjash <jashanpreet.singh.1004@gmail.com>
2025-09-18 16:27:38 +0530

    Fixed cors

--- Backend/config/corsConfig.js
+++ Backend/config/corsConfig.js
@@ -1,6 +1,4 @@
 export const corsConfig = {
-    origin: process.env.NODE_ENV === 'production'
-        ? ["https://sih-impact-crew.vercel.app/"]
-        : ["http://localhost:5173"],
+    origin: ["https://sih-impact-crew.vercel.app", "http://localhost:5173"],
     credentials: true
 }; 
