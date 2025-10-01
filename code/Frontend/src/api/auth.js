10b4cd67180c2e52813ff2d8dacd967855ffbe3e
MAYANk-2705 <168637694+Retarded23@users.noreply.github.com>
2025-10-01 14:35:21 +0530

    transferownership routes correction

--- code/Frontend/src/api/auth.js
+++ code/Frontend/src/api/auth.js
@@ -1,52 +1,54 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_APP_API_BASE;
-const OWNER_BASE = import.meta.env.VITE_APP_OWNER_BASE;
-const Admin_BASE = import.meta.env.VITE_APP_ADMIN_BASE;
+const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
 export const registerUniversity = async (data) => {
-    return axios.post(`${API_BASE}/registerUniversity`, data, { withCredentials: true });
+    return axios.post(`${BACKEND_URL}/auth/registerUniversity`, data, { withCredentials: true });
 };
 export const login = async (data) => {
-    
-    return axios.post(`${API_BASE}/login`, data, { withCredentials: true });
+
+    return axios.post(`${BACKEND_URL}/auth/login`, data, { withCredentials: true });
 };
 
 export const logout = async () => {
-    return axios.post(`${API_BASE}/logout`, {}, { withCredentials: true });
+    return axios.post(`${BACKEND_URL}/auth/logout`, {}, { withCredentials: true });
 };
 
 export const verify = async () => {
-    return axios.get(`${API_BASE}/verify`, { withCredentials: true });
+    return axios.get(`${BACKEND_URL}/auth/verify`, { withCredentials: true });
 };
 
 //OWNER
 export const registerUser = async (data) => {
-    return axios.post(`${OWNER_BASE}/registerUser`, data, { withCredentials: true });
+    return axios.post(`${BACKEND_URL}/owner/registerUser`, data, { withCredentials: true });
 };
 
 export const removeUser = async (data) => {
-// data: { userId }
-    return axios.post(`${OWNER_BASE}/removeUser`, data, { withCredentials: true });
+    return axios.post(`${BACKEND_URL}/owner/removeUser`, data, { withCredentials: true });
 };
 
 export const userList = async () => {
-return axios.get(`${OWNER_BASE}/listUsers`, { withCredentials: true });
+return axios.get(`${BACKEND_URL}/owner/listUsers`, { withCredentials: true });
 };
 
 export const transferOwner = async (data) => {
-return axios.post(`${OWNER_BASE}/transferOwner`, data, { withCredentials: true });
+return axios.post(`${BACKEND_URL}/owner/transferOwnership`, data, { withCredentials: true });
 };
 
 //ADMINS
 export const sendfile=async(data)=>{
-return axios.post(`${Admin_BASE}/storedatalinks`,data,{ withCredentials: true });
+return axios.post(`${BACKEND_URL}/admin/storedatalinks`,data,{ withCredentials: true });
 }
 
 export const predictDropout = async () => {
-return axios.get(`${Admin_BASE}/predictdropout`, { withCredentials: true });
+return axios.get(`${BACKEND_URL}/admin/predictdropout`, { withCredentials: true });
 };
 
 export const refreshPrediction = async () => {
-return axios.get(`${Admin_BASE}/refreshPrediction`, { withCredentials: true });
+return axios.get(`${BACKEND_URL}/admin/refreshPrediction`, { withCredentials: true });
 };
+
+export const sendemail=async(data)=>{
+return axios.post(`${BACKEND_URL}/email/send`,data,{ withCredentials: true });
+}
+
